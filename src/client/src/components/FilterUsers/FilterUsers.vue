2cba4f6a062756793bffc9551e3ac4744fc80fdd
FedeSca001 <familadore@gmail.com>
2022-07-20 13:25:50 +0200

    filter

--- src/client/src/components/FilterUsers/FilterUsers.vue
+++ src/client/src/components/FilterUsers/FilterUsers.vue
@@ -1,32 +1,53 @@
 <template>
-  <h2>filter</h2>
-  <input type="text" @input="(e)=>{filterByName(e.target.value);}">
-  <ul>
-      <li v-for="item in filteredOut" :key="item.id">{{item.name}}</li>
-  </ul>
+        <h2 class="title">filter</h2>
+        <input
+                type="text"
+                placeholder="Name filter"
+                @input="
+                        (e) => {
+                                filterByName(e.target.value);
+                        }
+                "
+        />
+        <ul>
+                <li v-for="item in filteredOut" :key="item.id" class="liItem">
+                        Name: {{ item.name }} | | Age: {{ item.age }} | | Dni:
+                        {{ item.dni }} | | Phone: {{ item.phone }} | | Mail:
+                        {{ item.mail }}
+                </li>
+        </ul>
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-            list: this.sendProp,
-            filteredOut: []
-        }
-    },
-    props: ['sendProp'],
-    methods:{
-        filterByName(e){
-            let input = e
-            this.filteredOut = this.list.map((element)=>{
-                element.name === input
-            })
-            console.log('In='+e+'-----'+ 'filtredout'+this.filteredOut);
-        }
-    }
-}
+        data() {
+                return {
+                        list: this.sendProp,
+                        filteredOut: [],
+                };
+        },
+        props: ["sendProp"],
+        methods: {
+                filterByName(e) {
+                        let input = e;
+                        this.filteredOut = this.list.filter((element) => {
+                                return element.name == input;
+                                //console.log(element.name + "---" + input);
+                        });
+                },
+        },
+};
 </script>
 
-<style>
-
+<style scoper>
+.title {
+        margin: 2vw 4vw;
+        font-size: 3vw;
+}
+.liItem {
+        font-size: 1.9vw;
+        font-weight: 700;
+        margin: 0.5vw 5vw;
+        list-style: none;
+}
 </style>
--- src/client/src/views/UsersMain/UsersMain.vue
+++ src/client/src/views/UsersMain/UsersMain.vue
@@ -1,62 +1,79 @@
-<template @change="get.getUsers(), console.log('uno')">
-  <button  @click="()=>{swichBtn = !swichBtn}" class="swich">Add User</button>
-  <button @click="()=>{swichFilter = !swichFilter}">Filter</button>
-  <AddUser v-if="swichBtn" />
-  <FilterUsers v-if="swichFilter" :sendProp="get.listUsers"/>
-  <ul v-else>
-  <h2 class="title" >Users</h2>
-    <li v-for="item in get.listUsers" 
-      :key="item.name"
-      class="liItem">
-        Name: {{item.name}} | |
-        Age: {{item.age}} | |
-        Dni: {{item.dni}} | |
-        Phone: {{item.phone}} | |
-        Mail: {{item.mail}}
-    </li>
-  </ul>
+<template @change="get.getUsers()">
+        <button
+                @click="
+                        () => {
+                                swichBtn = !swichBtn;
+                        }
+                "
+                class="swich"
+        >
+                Add User
+        </button>
+        <button
+                @click="
+                        () => {
+                                swichFilter = !swichFilter;
+                        }
+                "
+        >
+                Filter
+        </button>
+        <AddUser v-if="swichBtn" />
+        <FilterUsers v-if="swichFilter" :sendProp="get.listUsers" />
+        <ul v-else>
+                <h2 class="title">Users</h2>
+                <li
+                        v-for="item in get.listUsers"
+                        :key="item.name"
+                        class="liItem"
+                >
+                        Name: {{ item.name }} | | Age: {{ item.age }} | | Dni:
+                        {{ item.dni }} | | Phone: {{ item.phone }} | | Mail:
+                        {{ item.mail }}
+                </li>
+        </ul>
 </template>
 
 <script>
-import AddUser from '@/components/AddUser/AddUser.vue'
-import FilterUsers from '@/components/FilterUsers/FilterUsers.vue'
-import { useUsers } from '../../store/users'
+import AddUser from "@/components/AddUser/AddUser.vue";
+import FilterUsers from "@/components/FilterUsers/FilterUsers.vue";
+import { useUsers } from "../../store/users";
 export default {
-  components: { AddUser, FilterUsers },
-  data(){
-    return {
-      swichBtn: false,
-      swichFilter: false
-    }
-  },
-  setup(){
-    const get = useUsers();
-    get.getUsers();
-    return{
-      get
-    }
-  }
-}
+        components: { AddUser, FilterUsers },
+        data() {
+                return {
+                        swichBtn: false,
+                        swichFilter: false,
+                };
+        },
+        setup() {
+                const get = useUsers();
+                get.getUsers();
+                return {
+                        get,
+                };
+        },
+};
 </script>
 
 <style scoped>
-.swich{
-  margin: 2vw 4vw;
-  font-size: 2.3vw;
-  padding: 1.8vw;
-  border: solid black 2.5px;
-  background-color: brown;
-  color: white;
-  border-radius: 2.5vw;
+.swich {
+        margin: 2vw 4vw;
+        font-size: 2.3vw;
+        padding: 1.8vw;
+        border: solid black 2.5px;
+        background-color: brown;
+        color: white;
+        border-radius: 2.5vw;
 }
-.title{
-    margin: 2vw 4vw;
-    font-size: 3vw;
+.title {
+        margin: 2vw 4vw;
+        font-size: 3vw;
 }
-.liItem{
-    font-size: 1.9vw;
-    font-weight: 700;
-    margin: 0.5vw 5vw;
-    list-style: none;
+.liItem {
+        font-size: 1.9vw;
+        font-weight: 700;
+        margin: 0.5vw 5vw;
+        list-style: none;
 }
 </style>
